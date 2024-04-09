9ed15961fca2c2fc3c78134b3dae70714d80bb01
fernandolv123 <ferlorenzovazquez@gmail.com>
2024-04-09 21:44:30 +0200

    V1.0 finished, players can now jump

--- Network_Transform/Assets/Scripts/GameManager.cs
+++ Network_Transform/Assets/Scripts/GameManager.cs
@@ -41,18 +41,18 @@ public class GameManager : MonoBehaviour
 
         static void SubmitNewPosition()
         {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
+            if (GUILayout.Button(NetworkManager.Singleton.IsHost ? "Jump": NetworkManager.Singleton.IsServer ? "Jump But Better Cause you are server" : "Jump"))
             {
                 if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient )
                 {
                     foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().Move();
+                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().Jump();
                 }
                 else
                 {
                     var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
                     var player = playerObject.GetComponent<Player>();
-                    player.Move();
+                    player.Jump();
                 }
             }
         }
--- Network_Transform/Assets/Scripts/Player.cs
+++ Network_Transform/Assets/Scripts/Player.cs
@@ -3,68 +3,77 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class Player : NetworkBehaviour
-{
-    protected int upwardsVelocity;
-    protected int horizontalVelocity;
+{   
+    protected int zVelocity;
+    protected int xVelocity;
+    private Rigidbody rb;
     public float speed = 4;
-    //cambiado en el clon
+    public float jumpForce;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public override void OnNetworkSpawn()
     {
         if (IsOwner)
         {
-            //Move();
+            SubmitInitialPositionRPC();
         }
     }
 
-    public void Move()
+    private void Movement()
     {
-    if(Input.GetKey(KeyCode.W)){
-        upwardsVelocity = 1;
-    } else if(Input.GetKey(KeyCode.S)){
-        upwardsVelocity = -1;
-    } else {
-        upwardsVelocity = 0;
-    }
-
-    if(Input.GetKey(KeyCode.A)){
-        horizontalVelocity = 1;
-    } else if(Input.GetKey(KeyCode.D)){
-        horizontalVelocity = -1;
-    } else {
-        horizontalVelocity = 0;
-    }
-        //transform.Translate(Vector3.forward * upwardsVelocity*Time.fixedDeltaTime * speed + Vector3.right * horizontalVelocity*Time.fixedDeltaTime* speed,Space.Self);
-        //Vector3 newPosition = transform.position + Vector3.forward * upwardsVelocity*Time.fixedDeltaTime * speed + Vector3.right * horizontalVelocity*Time.fixedDeltaTime* speed;
-        //transform.position = newPosition;
-        //El cliente no puede escribir en las networks variables, son readonly, solo el servidor puede escribir en ellas
-        //Position.Value = transform.position;
-        //Vector3 newPosition = transform.position + new Vector3(upwardsVelocity*speed,0,horizontalVelocity*speed);
-        //SubmitPositionRequestServerRpc(newPosition);
+        zVelocity = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+        xVelocity = Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
 
         //float moveX = Input.GetAxis("Horizontal");
         //float moveZ = Input.GetAxis("Vertical");
 
         //Vector3 newPosition = new Vector3(moveX,0,moveZ);
         //transform.position += newPosition * speed * Time.deltaTime;
-
-        //transform.Translate(Vector3.forward * moveX*Time.fixedDeltaTime * speed + Vector3.right * moveZ*Time.fixedDeltaTime* speed,Space.Self);
-        SubmitPositionRequestServerRpc(upwardsVelocity,horizontalVelocity);
+        SubmitPositionRequestServerRpc(zVelocity,xVelocity);
     
     }
 
-    [Rpc(SendTo.Server)]
-    void SubmitPositionRequestServerRpc(float moveX,float moveZ)
-    {
-        //Position.Value = newPosition;
-        transform.Translate(Vector3.forward * moveX*Time.fixedDeltaTime * speed + Vector3.right * moveZ*Time.fixedDeltaTime* speed,Space.Self);
+    public void Jump() {
+        Debug.Log("Salta");
+        SubmitJumpRPC();
     }
 
+    [Rpc(SendTo.Server)]
+    void SubmitPositionRequestServerRpc(float moveZ,float moveX)
+    {
+        //movemos el player en servidor
+        transform.Translate(Vector3.forward * moveZ*Time.fixedDeltaTime * speed + Vector3.right * moveX*Time.fixedDeltaTime* speed,Space.Self);
+    }
+    //Creamos un método para cambiar la posición inicial y ponerlo al nivel del plano
+    [Rpc(SendTo.Server)]
+    void SubmitInitialPositionRPC(){
+        transform.position = transform.position + Vector3.up;
+    }
+
+    //Creamos un método para mandar al servidor la orden de saltar
+    [Rpc(SendTo.Server)]
+    void SubmitJumpRPC(){
+        rb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
+    }
+
+    //El FixedUpdate no es llamado cada frame
     void FixedUpdate()
     {
         if (IsOwner){
-            Move();
+            Movement();
+        }
+    }
+    void Update(){
+        if (IsOwner){
+            if (Input.GetKeyDown(KeyCode.Space)){
+                Jump();
+            }
         }
     }
 }
